69543727e22d8cea47921615e97b48ea4218cbc8
jmasengeshomsft <86074746+jmasengeshomsft@users.noreply.github.com>
2023-05-30 14:21:11 +0000

    initial

---
+++ main.tf
@@ -0,0 +1,224 @@
+module "ssh-key" {
+  source         = "../ssh-key"
+  public_ssh_key = var.public_ssh_key == "" ? "" : var.public_ssh_key
+}
+
+
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "aks"
+  kubernetes_version  = var.kubernetes_version
+  # private_dns_zone_id = var.private_dns_zone_id     
+
+  #security
+  private_cluster_enabled     = false
+  azure_policy_enabled        = true 
+  # open_service_mesh_enabled   = true  
+  sku_tier                    = var.sku_tier
+  oidc_issuer_enabled         = true
+
+
+  default_node_pool {
+    name                    = "default"
+    # node_count              = var.default_node_count
+    vm_size                 = var.default_vm_size
+    type                    = var.node_pool_type 
+    tags                    = var.tags
+    max_pods                = var.default_pool_max_pods
+    vnet_subnet_id          = var.aks_subnet_id
+    # pod_subnet_id           = var.workload_linux_subnet_id
+    enable_host_encryption  = false
+    enable_auto_scaling     = true
+    min_count               = var.default_node_count
+    max_count               = 10
+    zones                   = ["1","2","3"]
+    node_labels             = {
+      workload_type = "system"
+    }
+  }
+
+  linux_profile {
+    admin_username      = var.linux_admin_user
+    ssh_key {
+        key_data        = replace(var.public_ssh_key == "" ? module.ssh-key.public_ssh_key : var.public_ssh_key, "\n", "")
+    }      
+  }
+
+  identity {
+    type         = "SystemAssigned"
+  }
+
+  oms_agent  {
+    log_analytics_workspace_id = var.azurerm_log_analytics_workspace_id
+  }
+
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true
+    secret_rotation_interval = "2m"
+  }
+
+  role_based_access_control_enabled = true
+
+  # azure_active_directory_role_based_access_control{
+  #   managed = true
+  #   # tenant_id = var.tenant_id
+  #   admin_group_object_ids = ["a4e5ec83-a839-4d3a-9b73-718f04a7e892"]
+  #   azure_rbac_enabled = true
+  # }
+  
+  network_profile {
+      network_plugin      = var.network_plugin
+      # docker_bridge_cidr  = var.docker_bridge_cidr
+      # pod_cidr            = var.pod_cidr
+      service_cidr        = var.service_cidr
+      dns_service_ip      = var.dns_service_ip
+      outbound_type       = var.outbound_type
+      network_policy      = var.network_policy
+  }
+
+  storage_profile {
+    blob_driver_enabled = true
+    # disk_driver_version = "v2"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "workload_linux" {
+    name                    = "lightapps"
+    kubernetes_cluster_id   = azurerm_kubernetes_cluster.aks.id
+    # node_count              = var.workload_node_count
+    vm_size                 = var.workload_vm_size
+    tags                    = var.tags
+    max_pods                = var.default_pool_max_pods
+    vnet_subnet_id          = var.aks_subnet_id
+    # pod_subnet_id         = var.workload_linux_subnet_id
+    enable_host_encryption  = false
+    enable_auto_scaling     = true
+    os_type                 = var.workload_os_type
+    min_count               = var.workload_node_count
+    max_count               = 10
+    zones                   = ["1","2","3"]
+    node_labels = {
+      workload_type = "application_linux"
+    }
+  }
+
+
+# resource "azurerm_monitor_diagnostic_setting" "aks-logging" {
+#   name                                  = azurerm_kubernetes_cluster.aks.name
+#   target_resource_id                    = azurerm_kubernetes_cluster.aks.id
+#   log_analytics_workspace_id            = var.azurerm_log_analytics_workspace_id
+
+  #  log {
+  #   category = "csi-azuredisk-controller"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  #  log {
+  #   category = "csi-azurefile-controller"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  #  log {
+  #   category = "csi-snapshot-controller"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  #  log {
+  #   category = "cloud-controller-manager"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  #  log {
+  #   category = "kube-audit-admin"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+  
+  
+  # log {
+  #   category = "kube-scheduler"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  # log {
+  #   category = "kube-controller-manager"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  # log {
+  #   category = "cluster-autoscaler"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  # log {
+  #   category = "kube-audit"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  #   log {
+  #   category = "guard"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  # log {
+  #   category = "kube-apiserver"
+  #   enabled  = true
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+
+  # metric {
+  #   category = "AllMetrics"
+
+  #   retention_policy {
+  #     enabled = false
+  #   }
+  # }
+# }
+
---
+++ variables.tf
@@ -0,0 +1,158 @@
+variable "resource_group_name" {
+    description = "Application Gateway Resource Group"
+}
+
+variable "location" {
+    description = "cluster location"
+}
+
+variable "virtual_network_name" {
+    description = "The name of the vnet"
+}
+
+variable "vnet_resource_group_name" {
+    description = "The name of the vnet"
+}
+
+variable "aks_subnet_id" {
+    description = "The id of the subnet to attach to the udr"
+}
+
+variable "public_ssh_key" {
+  description = "A custom ssh key to control access to the AKS cluster"
+  default     = ""
+}
+
+variable "linux_admin_user" {
+    description = "admin user"
+    default     = "azureadminuser"
+}
+
+variable "aks_cluster_name" {
+    description = "The name of the cluster"
+}
+
+# variable "private_dns_zone_id" {
+#     description = "The ID of the DNS Zone for the Private Link"
+# }
+
+variable "kubernetes_version" {
+    description = "Kubernetes version"
+}
+
+variable "default_node_count" {
+    description = "Pool node count"
+    default     = 3
+}
+
+variable "workload_os_type" {
+    description = "The Os Type"
+    default     = "Linux"
+}
+
+variable "workload_node_count" {
+    description = "Pool node count"
+    default     = 2
+}
+
+variable "workload_linux_subnet_id" {
+    description = "The subnet for linux workload pods"
+}
+
+variable "azurerm_log_analytics_workspace_id" {
+    description = "The id of the log analytics woskpace"
+
+}
+
+variable "default_pool_max_pods" {
+    description = "The number of pods on the default node pool"
+    default     = 200
+}
+
+variable "workload_pool_max_pods" {
+    description = "The number of pods on the workload node pool"
+    default     = 200
+}
+
+variable "sku_tier" {
+  description = "Use Uptime SLA or Not"
+  default = "Free"
+}
+
+# variable "default_pod_subnet_id" {
+#     description = "The subnet for pods on the default pool"
+# }
+
+variable "default_vm_size" {
+    description = "VM Size"
+    default     = "Standard_D2_v2"
+}
+
+variable "workload_vm_size" {
+    description = "VM Size"
+    default     = "Standard_D2_v2"
+}
+
+variable "node_pool_type" {
+    description = "VM type: AvailabilitySet or VirtualMachineScaleSets"
+    default     = "VirtualMachineScaleSets"
+}
+
+# variable "service_principal_client_id" {
+#     description = "The client id for the service principal "
+# }
+
+# variable "service_principal_client_secret" {
+#     description = "The client secret for the service principal"
+# }
+
+
+variable "network_plugin" {
+    description = "The network plugin: Azure/Kubenet"
+    default     = "kubenet"
+}
+
+variable "network_policy" {
+    description = "The network plugin: Azure/Kubenet"
+    default     = "calico"
+}
+
+variable "docker_bridge_cidr" {
+    description = "Docker Address Space"
+    # default     = "172.17.0.1/16"
+}
+
+variable "pod_cidr" {
+    description = "Pod Address Space"
+    # default     = "10.244.0.0/16"
+}
+
+variable "service_cidr" {
+    description = "Service Address Space"
+    # default     = "10.240.0.0/16"
+}
+
+variable "dns_service_ip" {
+    description = "Service Address Space"
+    # default     = "10.240.0.10"
+}
+
+variable "outbound_type" {
+    description = "Out bound type: userDefinedRouting/loadBalancer"
+    default     = "userDefinedRouting"
+}
+
+variable "tenant_id" {
+    description = "Tenant aad"
+}
+
+variable "azure_aad_admin_group_id" {
+    description = "The default aad group"
+    default = "aa4e5ec83-a839-4d3a-9b73-718f04a7e892"
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  default     =  {
+  }
+}
